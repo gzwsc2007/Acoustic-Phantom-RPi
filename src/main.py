7e57de41937c4d9ce1237a17879db38277236a1f
Anson Wang <gzwsc2007@gmail.com>
2016-04-16 20:21:32 -0400

    Cleaned up main.py a bit...

--- src/main.py
+++ src/main.py
@@ -13,6 +13,34 @@ PACKET_SIZE = 64
 
 g_tracker_process = None
 g_servo_process = None
+g_airplay_process = None
+
+def shutdownTracker():
+    global g_tracker_process
+
+    if (g_tracker_process != None):
+        g_tracker_process.terminate()
+        g_tracker_process.wait()
+        g_tracker_process = None
+        print "Tracker Stopped"
+
+def shutdownServoServer():
+    global g_servo_process
+
+    if (g_servo_process != None):
+        g_servo_process.terminate()
+        g_servo_process.wait()
+        g_servo_process = None
+        print "Servo Server Stopped"
+
+def shutdownAirplay():
+    global g_airplay_process
+
+    if (g_airplay_process != None):
+        g_airplay_process.terminate()
+        g_airplay_process.wait()
+        g_airplay_process = None
+        print "Airplay Stopped"
 
 class AppServiceHandler(SocketServer.BaseRequestHandler):
     """
@@ -26,6 +54,7 @@ class AppServiceHandler(SocketServer.BaseRequestHandler):
     def handle(self):
         global g_tracker_process
         global g_servo_process
+        global g_airplay_process
 
         # self.request is the TCP socket connected to the client
         data = ""
@@ -51,51 +80,36 @@ class AppServiceHandler(SocketServer.BaseRequestHandler):
         if appCmd < APP_CMD_INDEX_LO or appCmd > APP_CMD_INDEX_HI:
             print "unknown command"
         elif appCmd == APP_CMD_ENABLE_ALL:
+            # Shutdown anything that is running
+            shutdownTracker()
+            shutdownServoServer()
+            shutdownAirplay()
+            
             # Enable AirPlay
-            subprocess.call(["sudo","service","shairport-sync","start"])
+            g_airplay_process = subprocess.Popen(["sudo","shairport-sync"])
             print "Airplay enabled"
 
             # Start the servo server
-            if (g_servo_process == None):
-                g_servo_process = subprocess.Popen(("python", "servoControllerSmooth.py"))
-                print "Servo Server enabled"
-                time.sleep(0.5)
-            else:
-                print "Servo Server already running"
+            g_servo_process = subprocess.Popen(("python", "servoControllerSmooth.py"))
+            time.sleep(0.5)
+            print "Servo Server enabled"
 
             # Start the color tracker
-            if (g_tracker_process == None):
-                avH = ord(data[5])
-                loH = ord(data[6])
-                hiH = ord(data[7])
-                avH2 = ord(data[8])
-                loH2 = ord(data[9])
-                hiH2 = ord(data[10])
-                args = ("./tracking/tracker.o", str(loH), str(hiH), str(loH2), str(hiH2))
-                g_tracker_process = subprocess.Popen(args)
-                print "Tracker enabled"
-            else:
-                print "Tracker already running"
+            avH = ord(data[5])
+            loH = ord(data[6])
+            hiH = ord(data[7])
+            avH2 = ord(data[8])
+            loH2 = ord(data[9])
+            hiH2 = ord(data[10])
+            args = ("./tracking/tracker.o", str(loH), str(hiH), str(loH2), str(hiH2))
+            g_tracker_process = subprocess.Popen(args)
+            print "Tracker enabled"
 
             success = True
         elif appCmd == APP_CMD_DISABLE_ALL:
-            # Disable AirPlay
-            subprocess.call(["sudo","service","shairport-sync","stop"])
-            print "Airplay disabled"
-
-            # Kill the color tracker
-            if (g_tracker_process != None):
-                g_tracker_process.terminate()
-                g_tracker_process.wait()
-                g_tracker_process = None
-                print "Tracker Stopped"
-
-            if (g_servo_process != None):
-                g_servo_process.terminate()
-                g_servo_process.wait()
-                g_servo_process = None
-                print "Servo Server Stopped"
-
+            shutdownTracker()
+            shutdownServoServer()
+            shutdownAirplay()
             success = True
 
         reply = str(bytearray([65, 67, 80, 84, 0] + [0]*59))
@@ -120,16 +134,8 @@ if __name__ == "__main__":
         print "[main] Exiting..."
 
     # Cleanup
-    if (g_tracker_process != None):
-        g_tracker_process.terminate()
-        g_tracker_process.wait()
-        g_tracker_process = None
-        print "Tracker Stopped"
-
-    if (g_servo_process != None):
-        g_servo_process.terminate()
-        g_servo_process.wait()
-        g_servo_process = None
-        print "Servo Server Stopped"
+    shutdownTracker()
+    shutdownServoServer()
+    shutdownAirplay()
 
 
